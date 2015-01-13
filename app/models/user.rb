4de997001882ea6910f7e5fc557a59e2584c69d7
Anil Bridgpal <bridgpal@gmail.com>
2015-01-12 17:00:09 -0800

    updated with 1 to many

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  has_many :books
+
   validates :email,
     presence: true,
     uniqueness: {case_sensitive: false}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150112215323) do
+ActiveRecord::Schema.define(version: 20150113005511) do
+
+  create_table "books", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "books", ["user_id"], name: "index_books_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email"
